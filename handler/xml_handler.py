63624c6398fe91eb76fc8cae26795fe89148b993
i-media-dev <mrk_dev@i-media.ru>
2025-08-08 18:02:34 +0300

    fixing methods

--- handler/xml_handler.py
+++ handler/xml_handler.py
@@ -4,8 +4,19 @@ from pathlib import Path
 
 class XMLHandler():
 
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        feeds_folder: str = 'temp_feeds',
+        new_feeds_folder: str = 'new_feeds'
+    ) -> None:
+        self.feeds_folder = feeds_folder
+        self.new_feeds_folder = new_feeds_folder
+
+    def _get_tree(self, file_name: str):
+        file_path = (
+            Path(__file__).parent.parent / self.feeds_folder / file_name
+        )
+        return ET.parse(file_path)
 
     def _indent(self, elem, level=0) -> None:
         i = '\n' + level * '  '
@@ -29,21 +40,29 @@ class XMLHandler():
         with open(file_path, 'w', encoding='utf-8') as f:
             f.write(formatted_xml)
 
-    def make_offers_unavailable(self, tree, offers_id_list, XML_file_name):
-        file_path = Path(__file__).parent.parent / 'new_feeds'
+    def make_offers_unavailable(
+        self,
+        feeds_list,
+        offers_id_list,
+        flag='false'
+    ) -> bool:
+        file_path = Path(__file__).parent.parent / self.new_feeds_folder
         file_path.mkdir(parents=True, exist_ok=True)
         try:
-            root = tree.getroot()
-            for offer in root.findall('.//offer'):
-                offer_id = offer.get('id')
+            file_name_list = [feed.split('/')[-1] for feed in feeds_list]
+            for file_name in file_name_list:
+                tree = self._get_tree(file_name)
+                root = tree.getroot()
+                for offer in root.findall('.//offer'):
+                    offer_id = offer.get('id')
 
-                if offer_id and offer_id in offers_id_list:
-                    offer.set('available', 'false')
-            tree.write(
-                f'{file_path}/{XML_file_name}',
-                encoding='utf-8',
-                xml_declaration=True
-            )
+                    if offer_id and offer_id in offers_id_list:
+                        offer.set('available', flag)
+                tree.write(
+                    f'{file_path}/new_{file_name}',
+                    encoding='utf-8',
+                    xml_declaration=True
+                )
             return True
 
         except Exception as e:
